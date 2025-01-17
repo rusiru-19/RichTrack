5616cab3b689279ed4ef87f1fba40999a04f0470
Rusiru Thamara Kaluarachchi <122989375+rusiru-19@users.noreply.github.com>
2025-01-17 20:27:45 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import { Footer } from '../components/footer';
 import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import axios, { AxiosError } from 'axios';
 
 export default function LandingPage() {
   const router = useRouter();
