8a97eeac463efde52f003febf867def7b03f8437
rusiru <evildev360@gmail.com>
2025-01-17 21:52:13 +0530

    yh the end

--- app/pages/student/dashboard/page.tsx
+++ app/pages/student/dashboard/page.tsx
@@ -1,52 +1,13 @@
 "use client";
-import React, { useEffect } from "react";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+import React from "react";
 
 function Dashboard() {
-  const router = useRouter();
 
-  useEffect(() => {
-    const auth = async () => {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        console.log("No token found. Redirecting to login.");
-        router.push("/");
-        return;
-      }
-
-      try {
-        const response = await axios.post(
-          "/api/auth",
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        console.log("Auth response:", response);
-
-        if (response.status !== 200) {
-          console.log("Authentication failed. Redirecting to login.");
-          router.push("/");
-        }
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-          const errorMessage = error.response?.data?.message || error.message;
-          console.error("Authentication error:", errorMessage);
-        } else {
-          console.error("Unexpected error:", error);
-        }
-        router.push("/");
-      }
-    };
-
-    auth();
-  }, [router]);
-
-  return <div>Dashboard Content</div>;
+  return (
+    <div className="p-6">
+<h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+    </div>
+  );
 }
 
 export default Dashboard;
--- next.conig.js
+++ next.conig.js
@@ -2,6 +2,8 @@ module.exports = {
     // Target serverless deployment
     target: 'serverless',
     // Enable experimental features
+    exclude: ['api'],
+
     experimental: {
       // Enable client-side rendering
       clientSideRendering: true,
